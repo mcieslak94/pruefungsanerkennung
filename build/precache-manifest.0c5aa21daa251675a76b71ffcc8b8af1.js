265369737c59778d756d333ece857fc6609023fd
mcieslak94 <meikecieslak94@gmail.com>
2020-09-22 12:39:00 +0200

    buildversuch

---
+++ build/precache-manifest.0c5aa21daa251675a76b71ffcc8b8af1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420b63d41d694de48568d0d5380a8f39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97eea61fb1800ef49dcc",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c509dd4a2d361218717c",
+    "url": "/static/css/main.6951b598.chunk.css"
+  },
+  {
+    "revision": "97eea61fb1800ef49dcc",
+    "url": "/static/js/2.8fbc9b00.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.8fbc9b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c509dd4a2d361218717c",
+    "url": "/static/js/main.10bc3a31.chunk.js"
+  },
+  {
+    "revision": "03720950758563aa8bad",
+    "url": "/static/js/runtime-main.1abb063a.js"
+  }
+]);
