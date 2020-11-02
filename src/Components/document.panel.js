4c3d9a0a4dfc856ae7fa7afc00bef8d34a086516
meikec <meikecieslak94@gmail.com>
2020-11-02 16:30:51 +0100

    Add vollständigbutton

--- src/Components/document.panel.js
+++ src/Components/document.panel.js
@@ -6,17 +6,20 @@ import '../App.css';
 export default class DocumentsPanel extends Component {
 
     state = { 
-        progressValue: 0
+        progressValue: 0,
+        completeAntrag: false,
+        completeGrades: false,
+        completeHandbuch: false
     }  
         
     getProgressValue = () => {
             var value = 0;
-            if(this.props.data.docAntrag && this.props.data.docNoten && this.props.data.docHandbuch){
+            if(this.props.data.docAntrag===2 && this.props.data.docNoten===2 && this.props.data.docHandbuch===2){
                 return 100;
             }
-            this.props.data.docAntrag ? value += 33.3 : value += 0;
-            this.props.data.docNoten ? value += 33.3 : value += 0;
-            this.props.data.docHandbuch ? value += 33.3 : value += 0;
+            this.props.data.docAntrag===2 ? value += 33.3 : value += 0;
+            this.props.data.docNoten===2 ? value += 33.3 : value += 0;
+            this.props.data.docHandbuch===2 ? value += 33.3 : value += 0;
     
             return value;
     };
@@ -33,12 +36,13 @@ export default class DocumentsPanel extends Component {
 
     toggleGrades = () => {
         if (this.props.data.docNoten === 0){
-            this.props.data.docNoten = 1
-            this.setState({docNoten: 1})
+                this.props.data.docNoten = 1
+                this.setState({docNoten: 1})
         } else {
-            this.props.data.docNoten = 0
-            this.setState({docNoten: 0})
-        }    }
+                this.props.data.docNoten = 0
+                this.setState({docNoten: 0})
+        }    
+    }
 
     toggleModul = () => {
         if (this.props.data.docHandbuch === 0){
@@ -47,7 +51,38 @@ export default class DocumentsPanel extends Component {
         } else {
             this.props.data.docHandbuch = 0
             this.setState({docHandbuch: 0})
-        }    }
+        }    
+    }
+
+    toggleAntragComplete = () => {
+        if (this.props.data.docAntrag === 1){
+            this.props.data.docAntrag = 2
+            this.setState({docAntrag: 2})
+        } else {
+            this.props.data.docAntrag = 1
+            this.setState({docAntrag: 1})
+        }
+    }
+
+    toggleGradesComplete = () => {
+        if (this.props.data.docNoten === 1){
+            this.props.data.docNoten = 2
+            this.setState({docNoten: 2})
+        } else {
+            this.props.data.docNoten = 1
+            this.setState({docNoten: 1})
+        }  
+    }
+
+    toggleModulComplete = () => {
+        if (this.props.data.docHandbuch === 1){
+            this.props.data.docHandbuch = 2
+            this.setState({docHandbuch: 2})
+        } else {
+            this.props.data.docHandbuch = 1
+            this.setState({docHandbuch: 1})
+        }  
+    }
     
     
     render = () => {
@@ -55,15 +90,27 @@ export default class DocumentsPanel extends Component {
         <div>
             
             <Row>
-                <Col>
+                <Col xs={2}>
                     <FormGroup>
                         <div className="documentChecks">
-                            <CustomInput disabled={this.props.disabled} checked={this.props.data.docAntrag} type="checkbox" 
-                            id="docAntrag" onChange={this.toggleAntrag} label="Antrag auf Prüfungsanerkennung"/>
-                            <CustomInput disabled={this.props.disabled} checked={this.props.data.docNoten} type="checkbox" 
+                            <CustomInput disabled={this.props.disabled} checked={this.props.data.docAntrag>0} type="checkbox" 
+                            id="docAntrag" onChange={this.toggleAntrag} label="Prüfungsanerkennungsantrag"/>
+                            <CustomInput disabled={this.props.disabled} checked={this.props.data.docNoten>0} type="checkbox" 
                             id="docNoten" onChange={this.toggleGrades} label="Notenspiegel"/>
-                            <CustomInput disabled={this.props.disabled} checked={this.props.data.docHandbuch} type="checkbox" 
+                            <CustomInput disabled={this.props.disabled} checked={this.props.data.docHandbuch>0} type="checkbox" 
                             id="docHandbuch" onChange={this.toggleModul} label="Modulhandbuch"/>
+                        </div>
+                    </FormGroup>
+                </Col>
+                <Col xs={3}>
+                    <FormGroup>
+                        <div className="documentComplete" disabled= {this.props.disabled}>
+                            <CustomInput disabled={!this.props.data.docAntrag>0 && this.props.disabled} checked={this.props.data.docAntrag>1} type="checkbox" 
+                            id="docAntragComplete" onChange={this.toggleAntragComplete} label="Vollständig?"/>
+                            <CustomInput disabled={!this.props.data.docNoten>0} checked={this.props.data.docNoten>1} type="checkbox" 
+                            id="docNotenComplete" onChange={this.toggleGradesComplete} label="Vollständig?"/>
+                            <CustomInput disabled={!this.props.data.docHandbuch>0} checked={this.props.data.docHandbuch>1} type="checkbox" 
+                            id="docHandbuchComplete" onChange={this.toggleModulComplete} label="Vollständig?"/>
                         </div>
                     </FormGroup>
                 </Col>
